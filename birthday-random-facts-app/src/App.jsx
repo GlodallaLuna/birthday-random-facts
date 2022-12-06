24827b1231efcf6d39db1777746a57289abcc3cd
Gloria Mancini <glorialmphoto@gmail.com>
2022-12-06 17:38:59 +0100

    add logic for the form

--- birthday-random-facts-app/src/App.jsx
+++ birthday-random-facts-app/src/App.jsx
@@ -13,34 +13,37 @@ function App() {
 
 
 
-function handleClick() {
-  fetch(`https://api.api-ninjas.com/v1/historicalevents?month=03&${factObj.day}`,{ headers: { 'X-Api-Key': 'QMr1gJuVcUTLnhez5mqnwA==p2xCPT1z7qNIwdvW'}})
-          .then(response => response.json())
-          .then(data => {
-            getRandomFact(data)
-          })
-        
-}
 
-function getRandomFact(factsArray) {
-  const randomNumber = Math.floor(Math.random() * factsArray.length)
-  const stringEvent = factsArray[randomNumber].event
-  setFactObj(prevFactObj => ({
-    ...prevFactObj,
-    event: stringEvent
-  }))
-  
-}
+  function handleClick() {
+    fetch(`https://api.api-ninjas.com/v1/historicalevents?month=${factObj.month}&day=${factObj.day}`,{ headers: { 'X-Api-Key': 'QMr1gJuVcUTLnhez5mqnwA==p2xCPT1z7qNIwdvW'}})
+            .then(response => response.json())
+            .then(data => {
+              getRandomFact(data)
+            })
+          
+  }
 
-function handleChange(event) {
-  const {name, value} = event.target
-  setFactObj(prevFactObj => ({
-    ...prevFactObj,
-    [name]: value
-  }))
+  function getRandomFact(factsArray) {
+    const randomNumber = Math.floor(Math.random() * factsArray.length)
+    const stringEvent = factsArray[randomNumber].event
+    setFactObj(prevFactObj => ({
+      ...prevFactObj,
+      event: stringEvent
+    }))
+    
+  }
+
+  function handleChange(event) {
+    const {name, value} = event.target
+    setFactObj(prevFactObj => ({
+      ...prevFactObj,
+      [name]: value
+    }))
+    
+  }
+
   
-}
-console.log(factObj.day)
+  
 
   return (
     <main>
@@ -50,7 +53,7 @@ console.log(factObj.day)
             <p className='form-text'>What's your name?</p>
             <input 
                     type="text"
-                    placeholder="Type your First Name..."
+                    placeholder="Type your first name..."
                     className="form-input "
                     name="firstName"
                     value={factObj.firstName}
@@ -60,7 +63,10 @@ console.log(factObj.day)
             <div className='container-birthday'>
                 <div className="container-birthday--input">
                     <p className='form-text--input'>Select Month</p>
-                    <select name=" " className='form-option form-input form-input--birthday'>
+                    <select value={factObj.month}
+                            onChange={handleChange}
+                            name="month" 
+                            className='form-option form-input form-input--birthday'>
                         <option value="">--</option>
                         <option value="01">January</option>
                         <option value="02">February</option>
@@ -70,7 +76,7 @@ console.log(factObj.day)
                         <option value="06">June</option>
                         <option value="07">July</option>
                         <option value="08">August</option>
-                        <option value="09">Sptember</option>
+                        <option value="09">September</option>
                         <option value="10">October</option>
                         <option value="11">November</option>
                         <option value="12">December</option>
--- birthday-random-facts-app/src/Fact.jsx
+++ birthday-random-facts-app/src/Fact.jsx
@@ -2,11 +2,20 @@ import './style.css'
 
 export default function Fact(props) { 
   const text = props.factText
-  console.log(text)
+
+  function numberToMonth(number) {
+    const index = Number(number) - 1
+   const months = [ "January", "February", "March", "April", "May", "June", 
+           "July", "August", "September", "October", "November", "December" ]
+    return months[index]
+  }
+
+  
+  
     return(
        <div className='fact-text'>
         <p className='fact-paragraph'>Hello <span className='bold'>{text.firstName}</span>!</p>
-        <p className='fact-paragraph'>On <span className='bold'>March {text.day}</span> this is what happened:</p>
+        <p className='fact-paragraph'>On <span className='bold'>{numberToMonth(text.month)} {text.day}</span> this is what happened:</p>
         <p className='fact-paragraph'> {text.event}</p>
        </div>
     )
